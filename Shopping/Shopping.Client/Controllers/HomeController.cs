9e6a785bea02eb419b77b876ac14125cb9880f0d
LAPTOP-3TK4TGQM\Mahmoud <mgafar@eshopworld.com>
2022-08-16 17:07:04 +0400

    fix serializtion

--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -1,22 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shopping.Client.Models;
 using System.Diagnostics;
-using Shopping.Client.Data;
+using System.Text.Json;
 
 namespace Shopping.Client.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , IHttpClientFactory _httpClientFactory)
         {
+            _httpClient = _httpClientFactory.CreateClient("ShoppingApi");
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(ProductContext.Products);
+            var response = await _httpClient.GetAsync("/products");
+            var content = await response.Content.ReadAsStringAsync();
+            var productList = JsonSerializer.Deserialize<IEnumerable<Product>>(content , new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            return View(productList);
         }
 
         public IActionResult Privacy()
--- Shopping/Shopping.Client/Program.cs
+++ Shopping/Shopping.Client/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient("ShoppingApi", cfg =>
+{
+    cfg.BaseAddress = new Uri("https://localhost:5001/");
+});
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
