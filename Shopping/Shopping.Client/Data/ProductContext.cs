b3ed36e9f09ca97505ee879b6970b359c2fa3998
Mahmoud Alaskalany <Mhmoudalaskalany@gmail.com>
2022-08-16 08:52:05 +0400

    add product context

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,35 @@
+﻿using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new()
+        {
+            new Product
+            {
+                Name = "IPhone x",
+                Description = "IPhone X mobile phone",
+                ImageFile = "product1.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product
+            {
+                Name = "Samsung 10",
+                Description = "Samsung  mobile phone",
+                ImageFile = "product2.png",
+                Price = 840.00M,
+                Category = "Smart Phone"
+            },
+            new Product
+            {
+                Name = "Mi Redmi Note 11",
+                Description = "Redmi mobile phone",
+                ImageFile = "product3.png",
+                Price = 750.00M,
+                Category = "Smart Phone"
+            }
+        };
+    }
+}
